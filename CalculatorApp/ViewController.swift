6ab7cb3448d25db1dd44827b711e13d86f0b44c4
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-14 00:29:58 +0530

    Added 2 functionalities for number operation

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -11,16 +11,43 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var displayLabel: UILabel!
-    var isFinishedTyping: Bool = true
+        
+    // below variable can now be used within this class
+    private var isFinishedTyping: Bool = true
+    
+    private var displayValue: Double{
+        get{
+            guard let number = Double(displayLabel.text!) else{
+                fatalError("Cannot convert display label text to Double")
+            }
+            return number
+        }
+        set{
+            displayLabel.text = String(newValue)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
     @IBAction func calculationButtonPressed(_ sender: UIButton) {
     isFinishedTyping = true
+        
+        if let calculationMethod = sender.currentTitle{
+            switch calculationMethod{
+            case "+/-":
+                displayValue = displayValue * -1
+            case "AC":
+                displayValue = 0
+            case "%":
+                displayValue = displayValue * 0.01
+            default:
+                print("else where")
+            }
+        }
+        
     }
     
     
@@ -31,6 +58,12 @@ class ViewController: UIViewController {
                 displayLabel.text = numberValue
             isFinishedTyping = false
             }else{
+                if numberValue == "."{
+                    let isInt = floor(displayValue) == displayValue
+                    if !isInt{
+                        return
+                    }
+                }
                 displayLabel.text = displayLabel.text! + numberValue
             }
         }
